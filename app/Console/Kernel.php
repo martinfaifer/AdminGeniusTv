caf758ffca2fce7185e1ceb846b788ca677ffaa4
martinfaifer <51059458+martinfaifer@users.noreply.github.com>
2023-11-17 11:43:19 +0100

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('nangu:fetch_isps')->daily();
+        $schedule->command('nangu:fetch_isps')->twiceDaily();
     }
 
     /**
