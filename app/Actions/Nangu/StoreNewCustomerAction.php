c625995b55f9a7105bea014f105f36ffce8739b6
martinfaifer <51059458+martinfaifer@users.noreply.github.com>
2023-11-27 08:34:05 +0100

    Update StoreNewCustomerAction.php

--- app/Actions/Nangu/StoreNewCustomerAction.php
+++ app/Actions/Nangu/StoreNewCustomerAction.php
@@ -13,62 +13,62 @@ class StoreNewCustomerAction
 {
     public function execute(object $formData, $ispCode): bool
     {
-        // try {
-        $subscriberCode = $formData->subscriberCode;
-        $puk = $this->get_puk($formData);
+        try {
+            $subscriberCode = $formData->subscriberCode;
+            $puk = $this->get_puk($formData);
 
-        (new WsdlCreateSubscriberService())->execute(subscriberCode: $subscriberCode, ispCode: $ispCode);
+            (new WsdlCreateSubscriberService())->execute(subscriberCode: $subscriberCode, ispCode: $ispCode);
 
-        $subscriptionStbAccountCode = (new WsdlCreateSubscriptionService())->execute(
-            subscriberCode: $subscriberCode,
-            subscriptionCode: $formData->subscriptionCode,
-            tariffCode: $formData->tariffCode,
-            localityCode: $formData->localityCode,
-            ispCode: $ispCode
-        );
-        $identityId = (new WsdlCreateIdentityService())->execute(
-            username: is_null($formData->identityName) ? $formData->subscriptionCode : $formData->identityName,
-            password: $formData->identityPassword,
-            ispCode: $ispCode
-        );
-
-        (new WsdlAssignIdentityService())->execute(
-            subscriptionStbAccountCode: $subscriptionStbAccountCode,
-            identityId: $identityId,
-            ispCode: $ispCode
-        );
-
-        (new WsdlEnableSubscriptionService())->execute(
-            subscriptionCode: $formData->subscriptionCode,
-            subscriptionStbAccountCode: $subscriptionStbAccountCode,
-            puk: $puk,
-            ispCode: $ispCode
-        );
-
-        if (!is_null($formData->modelCode) || !is_null($formData->serialNumber) || !is_null($formData->macAddress)) {
-            (new WsdlStbService())->store(
-                modelCode: $formData->modelCode,
-                serialNumber: $formData->serialNumber,
-                macAddress: $formData->macAddress,
+            $subscriptionStbAccountCode = (new WsdlCreateSubscriptionService())->execute(
+                subscriberCode: $subscriberCode,
+                subscriptionCode: $formData->subscriptionCode,
+                tariffCode: $formData->tariffCode,
+                localityCode: $formData->localityCode,
+                ispCode: $ispCode
+            );
+            $identityId = (new WsdlCreateIdentityService())->execute(
+                username: is_null($formData->identityName) ? $formData->subscriptionCode : $formData->identityName,
+                password: $formData->identityPassword,
                 ispCode: $ispCode
             );
 
-            // assing stb
-            (new WsdlStbService())->assign(
+            (new WsdlAssignIdentityService())->execute(
+                subscriptionStbAccountCode: $subscriptionStbAccountCode,
+                identityId: $identityId,
+                ispCode: $ispCode
+            );
+
+            (new WsdlEnableSubscriptionService())->execute(
                 subscriptionCode: $formData->subscriptionCode,
                 subscriptionStbAccountCode: $subscriptionStbAccountCode,
-                modelCode: $formData->modelCode,
-                serialNumber: $formData->serialNumber,
-                macAddress: $formData->macAddress,
+                puk: $puk,
                 ispCode: $ispCode
             );
+
+            if (!is_null($formData->modelCode) || !is_null($formData->serialNumber) || !is_null($formData->macAddress)) {
+                (new WsdlStbService())->store(
+                    modelCode: $formData->modelCode,
+                    serialNumber: $formData->serialNumber,
+                    macAddress: $formData->macAddress,
+                    ispCode: $ispCode
+                );
+
+                // assing stb
+                (new WsdlStbService())->assign(
+                    subscriptionCode: $formData->subscriptionCode,
+                    subscriptionStbAccountCode: $subscriptionStbAccountCode,
+                    modelCode: $formData->modelCode,
+                    serialNumber: $formData->serialNumber,
+                    macAddress: $formData->macAddress,
+                    ispCode: $ispCode
+                );
+            }
+
+            return true;
+        } catch (\Throwable $th) {
+
+            return false;
         }
-
-        return true;
-        // } catch (\Throwable $th) {
-
-        //     return false;
-        // }
     }
 
     protected function get_puk(object $formData): string
