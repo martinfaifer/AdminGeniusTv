2d0fd7def5b458e528d55e25e11223fef5232c5e
martinfaifer <51059458+martinfaifer@users.noreply.github.com>
2024-05-09 12:31:45 +0200

    bug fix when customer deletes subscription

--- app/Actions/Nangu/DeleteCustomerAction.php
+++ app/Actions/Nangu/DeleteCustomerAction.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Actions\Nangu;
 
 use App\Services\Nangu\Wsdl\StbAccountService;
@@ -24,143 +25,148 @@ class DeleteCustomerAction
 
         $subscriptions = $subscriberDetail['subscriber']['getSubscriptionsResponse'];
 
-        if (array_key_exists("subscriptionCode", $subscriptions['subscriptions'])) {
-            // JEDNA SUBSCRIPTION
-            foreach ($subscriptions as $subscription) {
-                $stbAccountsData = $stbAccountService->get(
-                    subscriptionCode: $subscription['subscriptionCode'],
-                    ispCode: $ispCode
-                );
-                // STB ACCOUNT SECTION
-                foreach ($stbAccountsData as $stbAccount) {
-                    $identities = $wsdlIdentityService->show($stbAccount['subscriptionStbAccountCode'], $ispCode);
-
-                    // UNASSIGN IDENTITY
-                    $wsdlIdentityService->unassign(
-                        subscriptionStbAccountCode: $stbAccount['subscriptionStbAccountCode'],
-                        identityId: $identities['identities']['identityId'],
-                        ispCode: $ispCode
-                    );
-
-                    sleep(2);
-                    // DELETE IDENTITY
-                    $wsdlIdentityService->delete(
-                        identityId: $identities['identities']['identityId'],
-                        ispCode: $ispCode
-                    );
-
-                    sleep(2);
-                    // STB SECTION
-                    $stbsData = $subscriptionService->show_stb(
+        try {
+            if (array_key_exists("subscriptionCode", $subscriptions['subscriptions'])) {
+                // JEDNA SUBSCRIPTION
+                foreach ($subscriptions as $subscription) {
+                    $stbAccountsData = $stbAccountService->get(
                         subscriptionCode: $subscription['subscriptionCode'],
-                        subscriptionStbAccountCode: $stbAccount['subscriptionStbAccountCode'],
                         ispCode: $ispCode
                     );
+                    // STB ACCOUNT SECTION
+                    foreach ($stbAccountsData as $stbAccount) {
+                        $identities = $wsdlIdentityService->show($stbAccount['subscriptionStbAccountCode'], $ispCode);
 
-                    sleep(2);
-                    // UNASSING STB ACCOUNT
-                    $stbAccountService->unassing(
-                        subscriptionCode: $subscription['subscriptionCode'],
-                        subscriptionStbAccountCode: $stbAccount['subscriptionStbAccountCode'],
-                        ispCode: $ispCode
-                    );
-
-
-                    if (array_key_exists("stb", $stbsData)) {
-                        sleep(2);
-                        // DELETE STB
-                        $nanguWsdlStbService->delete(
-                            serialNumber: $stbsData['stb']['serialNumber'],
-                            macAddress: $stbsData['stb']['macAddress'],
-                            ispCode: $ispCode
-                        );
-
-                    }
-                }
-
-                sleep(2);
-                // REMOVE SUBSCRIPTION
-                $subscriptionService->delete(
-                    subscriptionCode: $subscription['subscriptionCode'],
-                    ispCode: $ispCode
-                );
-            }
-
-
-
-        } else {
-            // MNOHO SUBSCRIPTONS
-            foreach ($subscriptions['subscriptions'] as $subscription) {
-                $stbAccountsData = $stbAccountService->get(
-                    subscriptionCode: $subscription['subscriptionCode'],
-                    ispCode: $ispCode
-                );
-                // STB ACCOUNT SECTION
-                foreach ($stbAccountsData['subscriptionStbAccounts'] as $stbAccount) {
-                    $identities = $wsdlIdentityService->show($stbAccount['subscriptionStbAccountCode'], $ispCode);
-                    sleep(2);
-                    if (array_key_exists('identityId', $identities['identities'])) {
-                        // UNASSIGN IDENTITY
-                        sleep(2);
-                        $wsdlIdentityService->unassign(
-                            subscriptionStbAccountCode: $stbAccount['subscriptionStbAccountCode'],
-                            identityId: $identities['identities']['identityId'],
-                            ispCode: $ispCode
-                        );
-
-                        // DELETE IDENTITY
-                        sleep(2);
-                        $wsdlIdentityService->delete(
-                            identityId: $identities['identities']['identityId'],
-                            ispCode: $ispCode
-                        );
-                    } else {
-                        // IDENTITIES SECTION
-                        foreach ($identities['identities'] as $identity) {
+                        if (array_key_exists('identities', $identities)) {
                             // UNASSIGN IDENTITY
-                            sleep(2);
                             $wsdlIdentityService->unassign(
                                 subscriptionStbAccountCode: $stbAccount['subscriptionStbAccountCode'],
-                                identityId: $identity['identityId'],
+                                identityId: $identities['identities']['identityId'],
                                 ispCode: $ispCode
                             );
 
-                            // DELETE IDENTITY
                             sleep(2);
+                            // DELETE IDENTITY
                             $wsdlIdentityService->delete(
-                                identityId: $identity['identityId'],
+                                identityId: $identities['identities']['identityId'],
+                                ispCode: $ispCode
+                            );
+
+                            sleep(2);
+                        }
+
+                        // STB SECTION
+                        $stbsData = $subscriptionService->show_stb(
+                            subscriptionCode: $subscription['subscriptionCode'],
+                            subscriptionStbAccountCode: $stbAccount['subscriptionStbAccountCode'],
+                            ispCode: $ispCode
+                        );
+
+                        sleep(2);
+                        // UNASSING STB ACCOUNT
+                        $stbAccountService->unassing(
+                            subscriptionCode: $subscription['subscriptionCode'],
+                            subscriptionStbAccountCode: $stbAccount['subscriptionStbAccountCode'],
+                            ispCode: $ispCode
+                        );
+
+
+                        if (array_key_exists("stb", $stbsData)) {
+                            sleep(2);
+                            // DELETE STB
+                            $nanguWsdlStbService->delete(
+                                serialNumber: $stbsData['stb']['serialNumber'],
+                                macAddress: $stbsData['stb']['macAddress'],
                                 ispCode: $ispCode
                             );
                         }
                     }
 
-                    // UNASSING STB ACCOUNT
                     sleep(2);
-                    $stbAccountService->unassing(
+                    // REMOVE SUBSCRIPTION
+                    $subscriptionService->delete(
                         subscriptionCode: $subscription['subscriptionCode'],
-                        subscriptionStbAccountCode: $stbAccount['subscriptionStbAccountCode'],
                         ispCode: $ispCode
                     );
-
-                    // STB SECTION
-                    sleep(2);
-                    $stbsData = $subscriptionService->show_stb(
+                }
+            } else {
+                // MNOHO SUBSCRIPTONS
+                foreach ($subscriptions['subscriptions'] as $subscription) {
+                    $stbAccountsData = $stbAccountService->get(
                         subscriptionCode: $subscription['subscriptionCode'],
-                        subscriptionStbAccountCode: $stbAccount['subscriptionStbAccountCode'],
                         ispCode: $ispCode
                     );
-
-                    if (array_key_exists("stb", $stbsData)) {
-                        // DELETE STB
+                    // STB ACCOUNT SECTION
+                    foreach ($stbAccountsData['subscriptionStbAccounts'] as $stbAccount) {
+                        $identities = $wsdlIdentityService->show($stbAccount['subscriptionStbAccountCode'], $ispCode);
                         sleep(2);
-                        $nanguWsdlStbService->delete(
-                            serialNumber: $stbsData['stb']['serialNumber'],
-                            macAddress: $stbsData['stb']['macAddress'],
+                        if (array_key_exists('identityId', $identities['identities'])) {
+                            // UNASSIGN IDENTITY
+                            if (array_key_exists('identities', $identities)) {
+                                sleep(2);
+                                $wsdlIdentityService->unassign(
+                                    subscriptionStbAccountCode: $stbAccount['subscriptionStbAccountCode'],
+                                    identityId: $identities['identities']['identityId'],
+                                    ispCode: $ispCode
+                                );
+
+                                // DELETE IDENTITY
+                                sleep(2);
+                                $wsdlIdentityService->delete(
+                                    identityId: $identities['identities']['identityId'],
+                                    ispCode: $ispCode
+                                );
+                            }
+                        } else {
+                            // IDENTITIES SECTION
+                            foreach ($identities['identities'] as $identity) {
+                                // UNASSIGN IDENTITY
+                                sleep(2);
+                                $wsdlIdentityService->unassign(
+                                    subscriptionStbAccountCode: $stbAccount['subscriptionStbAccountCode'],
+                                    identityId: $identity['identityId'],
+                                    ispCode: $ispCode
+                                );
+
+                                // DELETE IDENTITY
+                                sleep(2);
+                                $wsdlIdentityService->delete(
+                                    identityId: $identity['identityId'],
+                                    ispCode: $ispCode
+                                );
+                            }
+                        }
+
+                        // UNASSING STB ACCOUNT
+                        sleep(2);
+                        $stbAccountService->unassing(
+                            subscriptionCode: $subscription['subscriptionCode'],
+                            subscriptionStbAccountCode: $stbAccount['subscriptionStbAccountCode'],
                             ispCode: $ispCode
                         );
+
+                        // STB SECTION
+                        sleep(2);
+                        $stbsData = $subscriptionService->show_stb(
+                            subscriptionCode: $subscription['subscriptionCode'],
+                            subscriptionStbAccountCode: $stbAccount['subscriptionStbAccountCode'],
+                            ispCode: $ispCode
+                        );
+
+                        if (array_key_exists("stb", $stbsData)) {
+                            // DELETE STB
+                            sleep(2);
+                            $nanguWsdlStbService->delete(
+                                serialNumber: $stbsData['stb']['serialNumber'],
+                                macAddress: $stbsData['stb']['macAddress'],
+                                ispCode: $ispCode
+                            );
+                        }
                     }
                 }
             }
+        } catch (\Throwable $th) {
+            //throw $th;
         }
 
         sleep(3);
